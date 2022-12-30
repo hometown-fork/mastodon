6231d4b7ee73b668c61106491bc7e0ce397b6e5b
Darius Kazemi <darius@meedan.com>
2022-12-30 03:21:15 -0800

    Change hashtag icon to search
    Because this is where search lives.

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -68,7 +68,7 @@ class NavigationPanel extends React.Component {
         )}
 
         {showTrends ? (
-          <ColumnLink transparent to='/explore' icon='hashtag' text={intl.formatMessage(messages.explore)} />
+          <ColumnLink transparent to='/explore' icon='search' text={intl.formatMessage(messages.explore)} />
         ) : (
           <ColumnLink transparent to='/search' icon='search' text={intl.formatMessage(messages.search)} />
         )}
