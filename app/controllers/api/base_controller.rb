9fbcfe7d5113627fd0d1fc71b5e12b777d93a2f1
Christopher Harrington <chris@harrington.mn>
2023-04-09 22:34:38 -0500

    Move unless criteria into boolean method
    It's helpful to be able to ask the parent class whether we would disallow API access based on the current request (and hence whether we know the requesting user) rather than simply based on configuration. This doesn't appear to regress any existing functionality.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -133,7 +133,7 @@ class Api::BaseController < ApplicationController
   end
 
   def disallow_unauthenticated_api_access?
-    ENV['DISALLOW_UNAUTHENTICATED_API_ACCESS'] == 'true' || Rails.configuration.x.whitelist_mode
+    (ENV['DISALLOW_UNAUTHENTICATED_API_ACCESS'] == 'true' || Rails.configuration.x.whitelist_mode) unless current_user
   end
 
   private
